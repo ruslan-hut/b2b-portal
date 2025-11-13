671a9cf03c58758b299a748b25a8a66d56448c8d
egorh <egor.hoot@gmail.com>
2025-11-13 15:19:34 +0100

    Add production environment configuration for deployment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// This file will be modified during deployment by scripts/set-env.js
+// The apiUrl will be replaced with the value from API_URL environment variable
+export const environment = {
+  production: true,
+  apiUrl: 'prod-url' // Will be replaced during CI/CD
+};
